b1094a1bfb2975695fa0f55b210fc30895a4f99c
fm-okuno <okuno@freemake.co.jp>
2020-04-13 15:19:00 +0900

    プロジェクトを作成

---
+++ Localization/Localization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Localization
+//
+//  Created by 開発アカウント on 2020/04/13.
+//  Copyright © 2020 開発アカウント. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
