628f6233d5f6c15647a9d1c4d4d8e0c26e106f32
fm-okuno <okuno@freemake.co.jp>
2020-04-13 16:16:25 +0900

    絶対に挫折しないiPhone開発「超」入門 Chapter10-03　実習
    Main.storyboard
    　・labelとbuttonを配置。buttonを押下でHelloと表示される様にした。
    
    InfoPlist.strings
    　・日本語環境でアプリ名が日本語になるようにした。
    
    Localizable.string
    　・label押下で表示される文字列「Hello」が日本語環境で「こんにちは」と翻訳されるようにした
    
    Main.strings
    　・アプリ内のlabelとbuttonが日本語で表示されるようにした

--- Localization/Localization/ViewController.swift
+++ Localization/Localization/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func actionDisplayHelloButton() {
+        label.text = NSLocalizedString("Hello", comment: "")
+    }
 
 }
 
